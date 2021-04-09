9a52cbf1277579bf64625d5de7787825202f73d6
pheni-18 <pheni.engineer@gmail.com>
2021-04-09 22:54:17 +0900

    update: move api url to env file

--- lib/network.dart
+++ lib/network.dart
@@ -1,9 +1,11 @@
 import 'package:http/http.dart' as http;
-import 'constants.dart';
 import 'dart:convert';
 import 'prefectures.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Covid19APIClient {
+  final String apiURL = env['API_URL'];
+
   Future<List<Prefecture>> getAll(String date) async {
     final String url = '$apiURL/all';
     http.Response response = await http.get('$url?date=$date');
